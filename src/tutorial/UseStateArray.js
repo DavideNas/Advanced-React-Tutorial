c23a69618491c900a0e61cb2e9023616c046fccf
DavideNas <d.naselli@gmail.com>
2021-03-23 12:37:29 +0100

    filter useState of Array by remove element

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -4,25 +4,41 @@ import Paper from "@material-ui/core/Paper";
 import { Button } from "@material-ui/core";
 import { data } from "./data";
 
+// UseState Array Example
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+  // filter function to show elements but the one removed
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
   return (
     <div className="container">
-      <h1>UseState Array Example</h1>
+      <h1>My characters</h1>
       {people.map((person) => {
         // create a structure from data file
         const { id, name } = person;
         console.log(person);
         return (
           <Box>
+            {/* print each element from data array */}
             <Paper style={{ padding: "0.25rem" }}>
-              <div key={id} className="item">
-                <h4>{name}</h4>
+              <div key={id}>
+                <h3>{name}</h3>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={() => removeItem(id)}
+                >
+                  Remove
+                </Button>
               </div>
             </Paper>
           </Box>
         );
       })}
+      {/* delete all list from screen view */}
       <Button
         variant="contained"
         color="secondary"
