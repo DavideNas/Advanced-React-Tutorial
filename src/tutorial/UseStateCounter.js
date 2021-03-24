a490c432c33ed58cffc6deb3c270593d028d8320
DavideNas <d.naselli@gmail.com>
2021-03-24 17:14:28 +0100

    Complex counter with timeout function

--- src/tutorial/UseStateCounter.js
+++ src/tutorial/UseStateCounter.js
@@ -4,6 +4,17 @@ import Button from "@material-ui/core/Button";
 // UseState Counter Example
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+  const [timer, setTimer] = useState(0);
+
+  const timedIncrease = () => {
+    setTimeout(() => {
+      // increase by 1 per click
+      setTimer((prevTimer) => {
+        return prevTimer + 1;
+      });
+    }, 1000);
+  };
+
   return (
     <div>
       <section style={{ margin: "4rem" }}>
@@ -36,6 +47,21 @@ const UseStateCounter = () => {
           variant="contained"
           color="primary"
           onClick={() => setValue(value + 1)}
+        >
+          Increase
+        </Button>
+      </section>
+
+      <section style={{ margin: "4rem" }}>
+        <h2>Timer Counter</h2>
+        <h1>{timer}</h1>
+        <Button
+          style={{
+            margin: "0.25rem",
+          }}
+          variant="contained"
+          color="primary"
+          onClick={timedIncrease}
         >
           Increase
         </Button>
