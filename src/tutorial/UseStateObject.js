fed1e67330b21f012e4fd4434f3579fd419d58ec
DavideNas <d.naselli@gmail.com>
2021-03-24 09:37:52 +0100

    Specific (multiple) state value

--- src/tutorial/UseStateObject.js
+++ src/tutorial/UseStateObject.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 
-// Use State Object Example
+// Specific (multiple) state value
 
 const UseStateObject = () => {
-  const changeMessage = () => {
-    // use of spread operator (keeps unchanged 'name' and 'age')
-    setPerson({ ...person, message: "oggi l'acqua è tiepida" });
-    // change only message (still not corrct)
-    //setPerson({ message: "oggi l'acqua è calda" });
-    // not correct method
-    //setPerson("oggi l'acqua è freddda");
-  };
-  // structured object with useState hook
   const [person, setPerson] = useState({
     name: "Kappa",
     age: 10,
     message: "ho sete di sangue",
   });
+
+  const [name, setName] = useState("Kappa");
+  const [age, setAge] = useState(5);
+  const [message, setMessage] = useState("l'acqua è fredda");
+
+  //
+  const changeMessage = () => {
+    // change a specific state value
+    setMessage("lacqua è tiepida");
+  };
+
+  // structured object with useState hook
   console.log(person);
   return (
     <div className="container">
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>“{person.message}„</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>“{message}„</h3>
       <Button variant="outlined" color="primary" onClick={changeMessage}>
         Change Message
       </Button>
