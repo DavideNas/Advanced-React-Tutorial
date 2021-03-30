341ab3634e048bd12d87a531181685b69039a1ce
DavideNas <d.naselli@gmail.com>
2021-03-30 11:19:45 +0200

    Use reducer, integration of the hook

--- src/tutorial/UseReducerSample.js
+++ src/tutorial/UseReducerSample.js
@@ -4,38 +4,51 @@ import {data} from "./data";
 
 // useReducer sample
 // used to add more structured form
-
+const reducer = (state,action) => {};
+const defaultState = {
+    //no records printed
+    //people:[],
+    //print array
+    people:data,
+    isModalOpen:false,
+    modalContent:"hello world"
+}
 const UseReducerSample = () => {
     const [name, setName] = useState('');
-    const [people, setPeople] = useState(data);
-    const [showModal, setShowModal] = useState(false);
+    // superseded by 'useReducer'
+    // const [people, setPeople] = useState(data);
+    // const [showModal, setShowModal] = useState(false);
+
+    // useReducer init 'state' passing object 'defaultState' to it
+    // it's always assotiated to 'dispatcher'
+    const [state, dispatch] = useReducer(reducer, defaultState)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if(name){
-            setShowModal(true);
-            setPeople([...people,{id:new Date().getTime().toString(), name}]);
-            setName("");
+
         }
         else {
-            setShowModal(true)
         }
     }
     return (
         <>
             {/* render component Modal if 'showModal' is true */}
-            {showModal && <Modal />}
+            {/* showModal && <Modal />*/}
+            {/* read boolean of modalContent using 'state' */}
+            {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
             <form onSubmit={handleSubmit}>
                 <div>
-                    <input 
-                        type="text" 
-                        value={name} 
+                    <input
+                        type="text"
+                        value={name}
                         onChange={(e)=> setName(e.target.value)}
                     />
                 </div>
                 <button type="submit">add</button>
             </form>
-            {people.map((person) => {
+            {/* print of array */}
+            {state.people.map((person) => {
                 return <div key={person.id}>
                     <h3>{person.name}</h3>
                 </div>
